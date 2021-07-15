c64ad8c6c69e662ae825a23feffe8a352dd76e55
LeChe27 <86964103+LeChe27@users.noreply.github.com>
2021-07-15 16:30:53 +0200

    Add files via upload

---
+++ chromeSerialBridge.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+* Extension unique id to start the comunication.
+* Extension id is supposed to remain static. It should never change, even when developing.
+*/
+var chromeSerialExtensionId = 'hgfifalikalcfmbphbkgdhbfollppfcn';
+
+chrome.runtime.sendMessage(chromeSerialExtensionId, {info: "TEST"}, function(response) {
+    console.log(response)
+});
+
+console.log("TODO 'chromeSerial.js'");
+
+
+/*
+if ("serial" in navigator) {
+    console.log("serial")
+    // The Web Serial API is supported.
+    //const ports = await navigator.serial.getPorts();
+    navigator.serial.getPorts(function(devices) {
+        console.log(devices);
+    });
+}
+*/
