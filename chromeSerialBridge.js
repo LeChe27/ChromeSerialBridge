b51007e7c6cc255c4cb17aa45d813e94801aa6b1
LeChe27 <le_che_27@hotmail.com>
2021-07-22 15:34:48 +0200

    .

--- chromeSerialBridge.js
+++ chromeSerialBridge.js
@@ -8,8 +8,7 @@ window.document.addEventListener("wallet-event", async () => {
         console.log(port)
         //document.dispatchEvent(new CustomEvent("wallet-connect", { detail: { result: 'OK', info: 'WALT3_GATEWAY' } }));
 
-        const serial_iframe = document.getElementById('ChromeSerialBridgeFrame')
-        serial_iframe.contentWindow.document.dispatchEvent(new CustomEvent("wallet-connect", { detail: { result: 'OK', info: 'WALT3_GATEWAY' } }));
+        document.dispatchEvent(new CustomEvent("wallet-connect", { detail: { result: 'OK', info: 'WALT3_GATEWAY' } }));
     } catch (e) {
         console.error(e)
     }
