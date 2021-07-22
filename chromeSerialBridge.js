4933f8acf7b10d6f0aa63fb8580d09c40ec00b3e
LeChe27 <le_che_27@hotmail.com>
2021-07-22 15:17:02 +0200

    .

--- chromeSerialBridge.js
+++ chromeSerialBridge.js
@@ -1,33 +1,15 @@
 'use strict';
 
 
-async function connectMCU() {
-    try {
-        const port = await navigator.serial.requestPort();
-        console.log(port)
-    } catch (e) {
-        console.error(e)
-    }
-}
-
-
-/*
-addEventListener('mouseup', Handler);
-
-function Handler() {
-    alert('works');
-}
-*/
 window.document.addEventListener("wallet-event", async () => {
-    console.debug('chromeSerailBridge  wallet-event')
+    console.debug('chromeSerailBridge:  wallet-event')
     try {
         const port = await navigator.serial.requestPort();
         console.log(port)
+        document.dispatchEvent(new CustomEvent("wallet-connect", { detail: { result: 'OK', info: 'WALT3_GATEWAY' } }));
     } catch (e) {
         console.error(e)
     }
-    //connectMCU()
-    //document.dispatchEvent(new CustomEvent("wallet-connect", { detail: { result: 'OK', info: 'WALT3_GATEWAY' } }));
 });
 
 /**
